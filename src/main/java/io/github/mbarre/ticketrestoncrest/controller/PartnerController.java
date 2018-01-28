68fe796906ce624e3d1e013efe8c9275f4714e32
mbarre <michele.barre@ville-noumea.nc>
2018-01-29 10:34:01 +1100

    fix #9 implements partners

---
+++ src/main/java/io/github/mbarre/ticketrestoncrest/controller/PartnerController.java
@@ -0,0 +1,26 @@
+package io.github.mbarre.ticketrestoncrest.controller;
+
+import io.github.mbarre.ticketrestoncrest.exception.InternalErrorException;
+import io.github.mbarre.ticketrestoncrest.model.Partner;
+import io.github.mbarre.ticketrestoncrest.service.PartnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PartnerController {
+
+    @Autowired
+    private PartnerService partnerService;
+
+    @GetMapping("/partners")
+    public List<Partner> getAllPartners() {
+        try {
+            return partnerService.getAllPartners();
+        } catch (Exception e) {
+            throw new InternalErrorException(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/io/github/mbarre/ticketrestoncrest/service/PartnerService.java
@@ -0,0 +1,42 @@
+package io.github.mbarre.ticketrestoncrest.service;
+
+import com.github.adriens.tickets.resto.nc.api.Affilie;
+import com.github.adriens.tickets.resto.nc.api.TicketsRestaurantsServiceWrapper;
+import io.github.mbarre.ticketrestoncrest.model.Partner;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PartnerService {
+
+    private final Logger log = LoggerFactory.getLogger(AccountService.class);
+    private TicketsRestaurantsServiceWrapper wrap;
+
+    public List<Partner> getAllPartners() throws Exception {
+
+        List<Partner> allPartners = new ArrayList<>();
+            List<Affilie> affilieList = TicketsRestaurantsServiceWrapper.getAffilies();
+            Partner newPartner;
+            for (Affilie affilie : affilieList) {
+                newPartner = new Partner();
+                newPartner.setName(affilie.getEnseigne());
+                if(affilie.getCategorie() != null && !affilie.getCategorie().isEmpty())
+                    newPartner.setCategory(StringUtils.capitalize(affilie.getCategorie().toLowerCase()));
+                if(affilie.getCuisine() != null && !affilie.getCuisine().isEmpty())
+                    newPartner.setFoodType(StringUtils.capitalize(affilie.getCuisine().toLowerCase()));
+                newPartner.setAddress(affilie.getFormattedAdress());
+                newPartner.setDistrict(affilie.getQuartier());
+                newPartner.setCity(affilie.getCommune());
+                newPartner.setPhone(Affilie.getFormattedTelephoneNumber(affilie.getTelephone()));
+                newPartner.setGoogleMapsUrl(Affilie.getGmapsURL(newPartner.getName()));
+                allPartners.add(newPartner);
+            }
+
+        return allPartners;
+    }
+}
